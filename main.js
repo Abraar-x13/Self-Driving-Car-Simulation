2ee4a61a6827db141e37217a05975314abed70d8
Abraar-x13 <Abraar13@proton.me>
2022-07-27 11:21:05 +0600

    Road and Camera

--- main.js
+++ main.js
@@ -1,20 +1,33 @@
 
 if (typeof window !== 'undefined') {
+
     const canvas = document.getElementById("myCanvas");
     canvas.width = 200;
     
     const ctx = canvas.getContext("2d");
-    const car = new Car(100, 100, 30, 50);
+    const road = new Road(canvas.width/2, canvas.width * 0.9);
+    const car = new Car(road.getLaneCentre(1), 100, 30, 50);
     car.draw(ctx);
 
     animate();
 
     function animate() {
+
         car.update();
+        
         canvas.height = window.innerHeight;
+        
+        ctx.save();
+        ctx.translate(0, -car.y + canvas.height * 0.70);
+
+        road.draw(ctx);
         car.draw(ctx);
+
+        ctx.restore();
         requestAnimationFrame(animate);
+    
     }
+
 }
   
 
---
+++ utils.js
@@ -0,0 +1,4 @@
+// LERP : Linear interpolation.
+function lerp(A, B, t) {
+    return A + (B - A) * t;
+}
